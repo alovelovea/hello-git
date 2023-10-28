0d058723ae240fbb0e8cc92af1794667178cd8dc
Minjae Park <2071162@hansung.edu>
2023-10-28 13:13:20 +0000

    added carol feature

--- Hello.java
+++ Hello.java
@@ -4,6 +4,6 @@ public class Hello
 	public static void main(String[] args )
 	{
 		System.out.println("Hello.world from GIT!");
-        System.out.println("asdChange after First commit!!")
+        System.out.println("asdChasdasdnge after First commit!!")
 	}
 }
